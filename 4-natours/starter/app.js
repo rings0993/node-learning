b300784f7152a9fbc761bb689df6c905563a66ea
rings0993 <ringsliu0993@gmail.com>
2022-08-27 22:52:58 +0800

    Try to connect to the DB

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,12 +3,15 @@ const express = require('express');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
 const app = express();
 
-app.use(express.json());
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(`${__dirname}/public/`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,28 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWARD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('DB connection successful');
+  });
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });

7b1f5c262f115e7659568c1d976d9a470e47223b
rings0993 <ringsliu0993@gmail.com>
2022-08-28 23:33:18 +0800

    Send First Data to BD

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -14,7 +14,6 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then((con) => {
     console.log(con.connection);
@@ -22,6 +21,35 @@ mongoose
   });
 // console.log(app.get('env'));
 // console.log(process.env);
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+
+  price: 498,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('❌ERROR: ', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

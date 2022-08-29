cfaaad38de151880db4b9b84ab753da6e47904ef
rings0993 <ringsliu0993@gmail.com>
2022-08-29 16:37:41 +0800

    Refactoring API Features

--- 4-natours/starter/controllers/tourControllers.js
+++ 4-natours/starter/controllers/tourControllers.js
@@ -1,9 +1,80 @@
 const Tour = require('./../models/tourModel');
 const express = require('express');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = 'price,ratingAverage';
+  req.query.fields = 'name,price,ratingAverage,difficulty';
+  console.log(req.query);
+  next();
+};
+
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((field) => delete queryObj[field]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+    // let query = Tour.find(JSON.parse(queryStr));
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.replace(/,/g, ' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('name -createdAt');
+    }
+
+    return this;
+  }
+
+  limit() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.replace(/,/g, ' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .paginate();
+    // console.log(features);
+
+    const tours = await features.query;
+    // const tour = await Tour.find().where('duration').equal(2)
+    // const tours = await Tour.find();
     res.status(200).json({
       status: 'success',
       requestTime: req.requestTime,
@@ -13,6 +84,7 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (err) {
+    console.log(err);
     res.status(404).json({
       status: 'fail',
       message: err,

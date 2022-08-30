a96f023c6c81dc7c4a648677e0f62dea3d815266
rings0993 <ringsliu0993@gmail.com>
2022-08-30 11:03:52 +0800

    Data Validation

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +10,9 @@ const tourSchema = new mongoose.Schema(
       index: true,
       unique: true,
       trim: true,
+      maxlengh: [40, 'The maximum length is 40 characters'],
+      minlength: [5, 'The minimum length is 5 characters'],
+      // validate: [validator.isAlpha, 'Name should contain only charaters'],
     },
     slug: String,
 
@@ -24,12 +28,31 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a difficulty'],
       default: 'medium',
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium or difficult',
+      },
     },
-    ratingAverage: { type: Number, default: 4.5 },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1'],
+      max: [5, 'Rating must be less than 5'],
+    },
     ratingQuantity: { type: Number, default: 0 },
     price: {
       type: Number,
       required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // NOT WORKING IN UPDATING
+          return val < this.price;
+        },
+        message: 'Discount price {VALUE} should below regular price',
+      },
     },
     discount: { type: Number, default: 0 },
     summary: { type: String, trim: true },

6d4ef06a8fa4cee6bb46cf10a978a42f495658b8
rings0993 <ringsliu0993@gmail.com>
2022-09-01 09:57:41 +0800

    Catching Error In Async Function

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,8 @@
 const morgan = require('morgan');
 const express = require('express');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errCountrollers');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
@@ -13,12 +15,28 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `Can not find ${req.originalUrl} on the server`,
+  // });
+  // const err = new Error(`Can not find ${req.originalUrl} on the server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  // next(err);
+
+  next(new AppError(`Can not find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;

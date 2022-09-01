14d56e08d62cdcf684e85bcd2ba4833300e622dc
rings0993 <ringsliu0993@gmail.com>
2022-09-01 12:23:53 +0800

    Error Handling

--- 4-natours/starter/controllers/errCountrollers.js
+++ 4-natours/starter/controllers/errCountrollers.js
@@ -1,10 +1,66 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate Fields: ${value}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationfailedDB = (err) => {
+  const errors = Object.values(err.errors).map((err) => err.message);
+
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  // const message = `Invalid input data`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    stack: err.stack,
+    error: err,
+    status: err.status,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message,
+      status: err.status,
+    });
+
+    // Programming or other unknown error: don't leak error details'
+  } else {
+    // 1) Log the error
+    console.log('Error💥', err);
+
+    // 2) Send the error
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'production') {
+    let error = Object.assign(err);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationfailedDB(error);
+    sendErrorProd(error, res);
+  } else if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
 };
--- 4-natours/starter/controllers/tourControllers.js
+++ 4-natours/starter/controllers/tourControllers.js
@@ -2,6 +2,7 @@ const Tour = require('./../models/tourModel');
 const express = require('express');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = 'price,ratingAverage';
@@ -31,6 +32,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('Tour not found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     requestTime: req.requestTime,
@@ -48,6 +54,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+
+  if (!tour) {
+    return next(new AppError('Tour not found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -60,6 +70,10 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
   // console.log(req.params);
   // console.log(tours[req.params.id]);
   const tour = await Tour.findByIdAndRemove(req.params.id);
+  if (!tour) {
+    return next(new AppError('Tour not found with that ID', 404));
+  }
+
   res.status(204).json({
     status: 'success',
   });

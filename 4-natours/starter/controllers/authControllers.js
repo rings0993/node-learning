c59a73111c5682c2986bd0b8f40182b6bb179c22
rings0993 <ringsliu0993@gmail.com>
2022-09-04 23:26:40 +0800

    Security for App

--- 4-natours/starter/controllers/authControllers.js
+++ 4-natours/starter/controllers/authControllers.js
@@ -9,16 +9,27 @@ const Email = require('./../utils/email');
 const { promisify } = require('util');
 const sendEmail = require('./../utils/email');
 
-const sighToken = (id) => {
+const signToken = (id) => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
 const createSendToken = (user, statusCode, res) => {
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from outputs
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<DATABASE_PASSWORD>',
@@ -23,6 +28,13 @@ const app = require('./app');
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
